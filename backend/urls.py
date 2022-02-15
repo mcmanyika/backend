6cfec295b1b6e1cba41a1c354c7ca1a3ce1f2ba3
mcmanyika <mcmanyika.com>
2022-02-15 14:27:10 -0600

    created lib

--- backend/urls.py
+++ backend/urls.py
@@ -4,11 +4,11 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, re_path, include
-from libs import views
+from lib import views
 from django.conf.urls import url
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('libs.urls')),
+    path('api/', include('lib.urls')),
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
