bba7f27ba711b6def37e0dd7ab0f793f5f44a438
mcmanyika <mcmanyika.com>
2022-03-24 13:21:06 -0500

    removed registration company

--- lib/models.py
+++ lib/models.py
@@ -82,8 +82,8 @@ class t_business_info(models.Model):
     idType = models.CharField(max_length=20, default="")
     idCard = models.ImageField(upload_to='business_docs')
     numberOfEmployees = models.CharField(max_length=50, default="")
-    registrationNumber = models.CharField(max_length=20, default="")
-    regCertificate = models.ImageField(upload_to='business_docs')
+    # registrationNumber = models.CharField(max_length=20, default="")
+    # regCertificate = models.ImageField(upload_to='business_docs')
     status = models.CharField(max_length=10, default="active")
     timestamp = models.DateTimeField(auto_now_add=True, auto_now=False)
 
